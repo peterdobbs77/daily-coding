c7fd7fee017ab9b500bf6dc0ae53090cde9931e4
Peter Dobbs <dobbsp8@gmail.com>
2020-01-17 12:37:36 -0600

    first pass attempt

---
+++ sum_check_k/sum_check_k.py
@@ -0,0 +1,22 @@
+# Let X be a random list of numbers
+# Let k be a random number
+# Return whether any two numbers from the list add up to k
+import numpy as np
+
+
+def check_if_sums(X, k):
+    for i in range(0, len(X)):
+        for j in range(i+1, len(X)):
+            l = X[i] + X[j]
+            print('{} + {} = {}'.format(X[i], X[j], l))
+            if(l == k):
+                return True
+    return False
+
+
+X = [15, 3, 10, 7]
+k = 17
+
+print(len(X))
+result = check_if_sums(X, k)
+print(result)
