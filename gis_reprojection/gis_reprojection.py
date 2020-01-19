87c9a3debc7f32d8c328670d2c15ee1c309ac09e
Peter Dobbs <dobbsp8@gmail.com>
2020-01-19 11:49:30 -0600

    getting geopandas to work

--- gis_reprojection/gis_reprojection.py
+++ gis_reprojection/gis_reprojection.py
@@ -1,32 +1,18 @@
-import os
+#import os
 import numpy as np
+import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import earthpy as et
+import requests
+import json
+from geopy.geocoders import Nominatim
 
-# url = "https://data.milwaukee.gov/api/3/action/datastore_search"
-# resourceId = '45c027b5-fa66-4de2-aa7e-d9314292093d'
-# limitApi = 10
-# # API call to Open MKE Data for liquor license data
-# url = url + "?resource_id={}&limit={}".format(resourceId, limitApi)
-# r = requests.get(url).json()
-# # print(json.dumps(r, indent=4))
-# print('Data query success:', r['success'])
+print('start program')
 
-# if(not r['success']):
-#     exit()
-
-# # If data query is successful, let's play with the data!
-# data = r['result']
-# df = pd.DataFrame(data['records'])
-
-# df['CITY'] = "Milwaukee, WI"  # add city, state to help geo location
-# print(df.head())
-
-# address = df[['HOUSE_NR', 'SDIR', 'STREET', 'STTYPE', 'CITY']].apply(
-#     lambda x: ' '.join(map(str, x)), axis=1)
-# print(address)
-
-citylimit = gpd.read_file('shape/citylimit/citylimit')
-
+citylimit = gpd.read_file('gis_reprojection/shape/citylimit/citylimit.shp')
 print(citylimit.crs)
+
+fig, ax = plt.subplots(figsize=(12, 8))
+citylimit.plot(ax=ax)  # (cmap='Greys', ax=ax, alpha=0.5)
+ax.set_title('Milwaukee, WI')
+plt.show()
