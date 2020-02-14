83783d34b6ee751be585077b07d7614484082215
Peter Dobbs <dobbsp8@gmail.com>
2020-02-14 17:20:25 -0600

    Update text_justify.py

--- palantir_text_justify/text_justify.py
+++ palantir_text_justify/text_justify.py
@@ -1,9 +1,19 @@
 
 def text_justify(words, k):
-    line_length = 0
-
+    line_length = len(words[0])
+    line_contents = [words[0]]
+    line_iterator = 0
     for i in range(len(words)):
-        line_length = len()
+        line_length += 1+len(words[i+1])  # add 1 for space
+        if (line_length < k):
+            line_contents.append(words[i+1])
+        else:
+            # TODO: collect words for line
+            nSpaces = k-
+
+            # setup for new line
+            line_length = len(words[i+1])
+            line_contents = [words[i+1]]
 
 
 words = ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
