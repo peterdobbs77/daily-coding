35071766f9fa68476b784262a52516dc1b37167b
peterdobbs77 <33101355+peterdobbs77@users.noreply.github.com>
2020-03-19 14:42:07 -0500

    Update 5_web_shop.sql

--- testdome/sql/5_web_shop.sql
+++ testdome/sql/5_web_shop.sql
@@ -1,7 +1,16 @@
---TABLE students
+--TABLE sellers
 --  id INTEGER PRIMARY KEY,
---  firstName VARCHAR(30) NOT NULL,
---  lastName VARCHAR(30) NOT NULL
+--  name VARCHAR(30) NOT NULL,
+--  rating INTEGER NOT NULL
+--
+--TABLE items
+--  id INTEGER PRIMARY KEY,
+--  name VARCHAR(30) NOT NULL,
+--  sellerId INTEGER REFERENCES sellers(id)
 
 
-select count(id) from students where firstname='John'
+select i.name,good_sellers.name from (
+  select * from sellers where rating>4
+) good_sellers
+join (select * from items) i
+on i.sellerId = good_sellers.id
