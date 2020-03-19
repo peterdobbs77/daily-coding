6e9b35b07a17a47136b5f99eed53fcf8ea78d05e
peterdobbs77 <33101355+peterdobbs77@users.noreply.github.com>
2020-03-19 14:38:48 -0500

    Update 2_enrollment.sql

--- testdome/sql/2_enrollment.sql
+++ testdome/sql/2_enrollment.sql
@@ -1,7 +1,7 @@
---TABLE students
---  id INTEGER PRIMARY KEY,
---  firstName VARCHAR(30) NOT NULL,
---  lastName VARCHAR(30) NOT NULL
+--TABLE enrollments
+--  id INTEGER NOT NULL PRIMARY KEY
+--  year INTEGER NOT NULL
+--  studentId INTEGER NOT NULL
 
 
-select count(id) from students where firstname='John'
+update enrollments set year=2015 where id>=20 and id<=100
