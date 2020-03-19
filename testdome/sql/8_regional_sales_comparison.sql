bac37237b3501b2278ef57e5cf71871c8e47effb
peterdobbs77 <33101355+peterdobbs77@users.noreply.github.com>
2020-03-19 15:06:00 -0500

    passes 75%
    ordering of the joins really matters!

--- testdome/sql/8_regional_sales_comparison.sql
+++ testdome/sql/8_regional_sales_comparison.sql
@@ -18,13 +18,15 @@
 --  employeeId INTEGER NOT NULL REFERENCES employees(id)
 
 
-select r.name as region, AVG(emp_amount) avg_emp_sales from(
+select r.name as region, AVG(emp_amount) as avg_emp_sales
+, (MAX(AVG(emp_amount)) OVER())-AVG(emp_amount) as diff
+from(
   select e.id as employeeId, e.stateid, SUM(IFNULL(sa.amount,0)) as emp_amount 
   FROM employees e
   LEFT OUTER join sales sa
   on e.id = sa.employeeId
   group by e.id, e.stateid
 ) agg
-left outer join states st on agg.stateId = st.id
-left outer join regions r on st.regionId = r.id
+join states st on agg.stateId = st.id
+join regions r on st.regionId = r.id
 group by r.name
