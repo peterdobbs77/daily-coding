602246431008ec3baf8a86d2ce8de0a2a85a02a3
peterdobbs77 <33101355+peterdobbs77@users.noreply.github.com>
2020-03-19 14:43:35 -0500

    Update 7_users_and_roles.sql

--- testdome/sql/7_users_and_roles.sql
+++ testdome/sql/7_users_and_roles.sql
@@ -1,7 +1,14 @@
---TABLE students
---  id INTEGER PRIMARY KEY,
---  firstName VARCHAR(30) NOT NULL,
---  lastName VARCHAR(30) NOT NULL
+--TABLE users
+--  id INTEGER NOT NULL PRIMARY KEY,
+--  userName VARCHAR(50) NOT NULL
+--
+--TABLE roles
+--  id INTEGER NOT NULL PRIMARY KEY,
+--  role VARCHAR(20) NOT NULL
 
 
-select count(id) from students where firstname='John'
+CREATE TABLE users_roles (
+  userId INTEGER NOT NULL REFERENCES users(id),
+  roleId INTEGER NOT NULL REFERENCES roles(id),
+  UNIQUE(userId,roleId)
+);
