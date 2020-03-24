e4c589b05b24f752928e94c4acb282b180d26e1e
peterdobbs77 <33101355+peterdobbs77@users.noreply.github.com>
2020-03-23 19:42:30 -0500

    Create marketing_costs.py

---
+++ testdome/data-science/marketing_costs.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn import linear_model
+
+def desired_marketing_expenditure(marketing_expenditure, units_sold, desired_units_sold):
+    """
+    :param marketing_expenditure: (list) A list of integers with the expenditure for each previous campaign.
+    :param units_sold: (list) A list of integers with the number of units sold for each previous campaign.
+    :param desired_units_sold: (integer) Target number of units to sell in the new campaign.
+    :returns: (float) Required amount of money to be invested.
+    """
+    return None
+
+#For example, with the parameters below, the function should return 250000.0
+print(desired_marketing_expenditure(
+    [300000, 200000, 400000, 300000, 100000],
+    [60000, 50000, 90000, 80000, 30000],
+    60000))
