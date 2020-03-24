2f4d7f3a18476905e73dad453a70eb05b27aea36
Peter Dobbs <dobbsp8@gmail.com>
2020-03-24 12:04:18 -0500

    Update stock_prices.py

--- testdome/data-science/stock_prices.py
+++ testdome/data-science/stock_prices.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 def most_corr(prices):
     """
     :param prices: (pandas.DataFrame) A dataframe containing each ticker's 
@@ -10,29 +11,30 @@ def most_corr(prices):
     """
     differences = prices.diff(axis=0)
     c = differences.corr().abs()
-    #print(c)
     c_triu = c.where(~np.tril(np.ones(c.shape)).astype(np.bool))
     c_triu = c_triu.stack()
+    print(c_triu)
     return c_triu.idxmax()
 
-#For example, the code below should print: ('FB', 'MSFT')
+
+# For example, the code below should print: ('FB', 'MSFT')
 print(most_corr(pd.DataFrame.from_dict({
-    'GOOG' : [
+    'GOOG': [
         742.66, 738.40, 738.22, 741.16,
         739.98, 747.28, 746.22, 741.80,
         745.33, 741.29, 742.83, 750.50
     ],
-    'FB' : [
+    'FB': [
         108.40, 107.92, 109.64, 112.22,
         109.57, 113.82, 114.03, 112.24,
         114.68, 112.92, 113.28, 115.40
     ],
-    'MSFT' : [
+    'MSFT': [
         55.40, 54.63, 54.98, 55.88,
         54.12, 59.16, 58.14, 55.97,
         61.20, 57.14, 56.62, 59.25
     ],
-    'AAPL' : [
+    'AAPL': [
         106.00, 104.66, 104.87, 105.69,
         104.22, 110.16, 109.84, 108.86,
         110.14, 107.66, 108.08, 109.90
