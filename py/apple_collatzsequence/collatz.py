fc5cdaa33ad681100389cc981c6886d09bbf908e
Peter Dobbs <dobbsp8@gmail.com>
2025-09-12 15:01:53 -0500

    delving deeper into understanding the sequence
    Starting to graph the sequences to assist in inspection and the separate BONUS question script completed

--- py/apple_collatzsequence/collatz.py
+++ py/apple_collatzsequence/collatz.py
@@ -6,10 +6,26 @@
 # Bonus: What input n <= 1000000 gives the longest sequence?
 
 
-def next_collatz(N):
+def next_collatz(N, sequence):
+    sequence.append(N)
     if N < 1:
         return None
-    if N % 2 == 0:
-        next_collatz(N/2)
+    if N == 1:
+        # print(f'N={N}, sequence has converged, sequence length is: {len(sequence)}')
+        return sequence
+    elif N % 2 == 0:
+        # print(f'N={N} is even, current sequence length is: {len(sequence)}')
+        next_collatz(N/2, sequence)
     else:
-        next_collatz(3*N + 1)
+        # print(f'N={N} is odd, current sequence length is: {len(sequence)}')
+        next_collatz(3*N + 1, sequence)
+    return sequence
+
+max_seq_len = 1
+for i in range(1,1000000):
+    i_sequence = next_collatz(i, [])
+    # print(f'When N={i}, Sequence Length is: {len(i_sequence)}')
+    if len(i_sequence) > max_seq_len:
+        max_seq_len = len(i_sequence)
+
+print(f'Max Sequence Length Where N <= 1000000 is: {max_seq_len}')
