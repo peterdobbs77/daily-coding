251a81f39157d35b2dbc910548ef77bebfca8cef
Peter Dobbs <dobbsp8@gmail.com>
2025-09-15 11:23:26 -0500

    removing debug print lines

--- py/square_shortest_substring/shortest_substring.py
+++ py/square_shortest_substring/shortest_substring.py
@@ -13,25 +13,18 @@ def shortest_substring_v1(str: string, chars: list):
     '''Given a string and a set of characters,
         return the shortest substring containing
         all the characters in the set.'''
-    
     temp_substr = str
     for i in range(len(str)):
         track_chars = [0]*len(chars) # keep track of if each char has been seen
         if str[i] in chars:
             track_chars[chars.index(str[i])] = 1
             for j in range(i+1, len(str)):
-                print('===')
-                print(f'{(i,j)}')
-                print(f'{(str[i],str[j])}')
-                print(f'{str[i:j+1]}')
                 if str[j] in chars:
                     track_chars[chars.index(str[j])] = 1
                 if sum(track_chars) == len(chars):
                     if j - i < len(temp_substr):
                         temp_substr = str[i:j+1]
                     break
-                print(f'{track_chars}')
-                print(f'{temp_substr}')
     return temp_substr
 
 # def position_difference(i_chars: list):
