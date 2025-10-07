1a4f15807f32418d0cb46e2b7fada7857944548a
Peter Dobbs <dobbsp8@gmail.com>
2025-10-07 13:16:49 -0500

    modularizing the manual conversion code to make it easier to test and manage

--- agents/knowledge_graph/fhir_to_rdf.py
+++ agents/knowledge_graph/fhir_to_rdf.py
@@ -13,14 +13,21 @@ DEPTH_LIMIT_ON = ['identifier', # 'subject',
 SKIP_KEYS = ['resourceType', 'id',  # because these two should already have been parsed
              'extension', 'telecom', 'address', 'maritalStatus', 'language', 'display']
 
+# map json key value to resource type
+MAP_KEY_TO_RESOURCE_TYPE = { # assumes that these keys are a reference to only one resource type (not necessarily true)
+    'subject': 'Patient',
+    'serviceProvider': 'Organization',
+    'encounter': 'Encounter'
+}
+
 class FhirGraph():
     '''A FHIR RDF representation of a FHIR JSON resource'''
 
-    def __init__(self, data) -> Graph:
+    def __init__(self, data, target_graph: Graph=None) -> Graph:
         '''Construct an RDF representaiton of FHIR Resource(s)
         :param data: source JSON representation of FHIR Resource(s)
         '''
-        self._graph = Graph()
+        self._graph = target_graph if target_graph is not None else Graph()
         self._graph.bind("fhir", FHIR)
         self._graph.bind("ex", EX)
         self._source = data
@@ -29,56 +36,44 @@ class FhirGraph():
             self._root_resource_type = 'Bundle'
         elif 'resourceType' in self._source:
             self._root_resource_type = self._source['resourceType']
-        self.generate()
-        return self._graph
-    
-    def add_reference_to_graph(self, reference_root, data_val: dict):
-        '''
-        :param reference_root:
-        :param data_val: passed along from 
-        '''
-        reference_node = BNode()
-        self._graph.add((reference_root, FHIR.reference, reference_node))
-        self._graph.add((reference_node, FHIR.value, Literal(data_val['reference'])))
 
-    def add_subject_reference_to_graph(self, root, data_key: str, data_val: dict):
-        '''assuming that subject is a reference to a Patient (not necessarily true)
-        :param root: root level of resource in RDF
+    def add_reference_to_resource(self, root_uri: URIRef, data_key: str, data_val: dict, referenced_resource_type: str):
+        '''
+        :param root_uri: root level of resource in RDF
         :param data_key: key for JSON object reference, should be 'subject'
         :param data_val: value for JSON object reference as dictionary
+        :param referenced_resource_type: must be from the FHIR Resource types
         '''
-        if data_key != 'subject':
-            return
-        entry = BNode()
-        self._graph.add((root, FHIR[data_key], entry))
-        self._graph.add((entry, RDF.type, FHIR.Patient))
-        self._graph.add((entry, FHIR.type, FHIR.Patient))
+        resource_reference = BNode()
+        self._graph.add((root_uri, FHIR[data_key], resource_reference))
+        self._graph.add((resource_reference, RDF.type, FHIR[referenced_resource_type]))
+        self._graph.add((resource_reference, FHIR.type, FHIR[referenced_resource_type]))
         if 'reference' in data_val:
-            # reference_bundle_entry = self._source['entry'][['fullUrl'] == value['reference']]
-            # print(reference_bundle_entry)
             # establish blank node reference
-            self.add_reference_to_graph(entry, data_val)
+            reference_node = BNode()
+            self._graph.add((resource_reference, FHIR.reference, reference_node))
+            self._graph.add((reference_node, FHIR.value, Literal(data_val['reference'])))
             # establish fhir:link
             reference_id = str.split(data_val['reference'], ':')[-1] # assuming reference is of the form "urn:uuid:#######"
-            self._graph.add((entry, FHIR.link, URIRef(EX['Patient' + '/' + reference_id])))
+            self._graph.add((resource_reference, FHIR.link, URIRef(EX[referenced_resource_type + '/' + reference_id])))
         if 'display' in data_val:
-            display_value = data_val['display']
-            self._graph.add((entry, FHIR.display, Literal(display_value)))
-        # print([x for x in value.items()]) # TEMP DEBUG LINE
+            self._graph.add((resource_reference, FHIR.display, Literal(data_val['display'])))
+        # print([x for x in data_val.items()]) # TEMP DEBUG LINE
 
-    def add_data_to_graph(self, data, root=None):
+    def add_data_to_graph(self, data, root_uri=None):
         '''Add data from FHIR resource to graph
         :param data: data from a FHIR resource to be added to the graph
-        :param root: root level of resource in RDF, for recursive calls
+        :param root_uri: root level of resource in RDF, for recursive calls
         '''
-        if root is None:
+        print(data)
+        if root_uri is None:
             resource_type = data['resourceType']
-            root = URIRef(EX[resource_type + '/' + data['id']])
+            root_uri = URIRef(EX[resource_type + '/' + data['id']])
             
             # Add basic properties for each resource
-            self._graph.add((root, RDF.type, FHIR[resource_type]))
-            self._graph.add((root, FHIR.nodeRole, FHIR.treeRoot))
-            self._graph.add((root, FHIR.resourceType, Literal(resource_type)))
+            self._graph.add((root_uri, RDF.type, FHIR[resource_type]))
+            self._graph.add((root_uri, FHIR.nodeRole, FHIR.treeRoot))
+            self._graph.add((root_uri, FHIR.resourceType, Literal(resource_type)))
         
         # Iterate over the attributes of the resource
         for key, value in data.items():
@@ -86,23 +81,31 @@ class FhirGraph():
                 continue
 
             if isinstance(value, dict):  # Handle nested objects
-                if key in ['subject']:
-                    # assuming that subject is a reference to a Patient (not necessarily true)
-                    self.add_subject_reference_to_graph(root, key, value)
-                if key not in DEPTH_LIMIT_ON:
-                    self.add_data_to_graph(value, root)  # Recursive call for nested objects
-                self._graph.add((root, FHIR[key], Literal(value)))  # Link to the nested resource
+                if key in MAP_KEY_TO_RESOURCE_TYPE.keys():
+                    self.add_reference_to_resource(root_uri, key, value, MAP_KEY_TO_RESOURCE_TYPE[key])
+                else:
+                    if key not in DEPTH_LIMIT_ON:
+                        self.add_data_to_graph(value, root_uri)  # Recursive call for nested objects
+                    self._graph.add((root_uri, FHIR[key], Literal(value)))  # Link to the nested resource
             elif isinstance(value, list):  # Handle lists
                 for item in value:
                     if isinstance(item, dict):  # If the item is a resource
                         if key not in DEPTH_LIMIT_ON:
-                            self.add_data_to_graph(item, root)  # Recursive call for nested resources
-                        self._graph.add((root, FHIR[key], Literal(item)))
+                            self.add_data_to_graph(item, root_uri)  # Recursive call for nested resources
+                        self._graph.add((root_uri, FHIR[key], Literal(item)))
                     else:
-                        self._graph.add((root, FHIR[key], Literal(item)))
+                        self._graph.add((root_uri, FHIR[key], Literal(item)))
             else:
-                self._graph.add((root, FHIR[key], Literal(value)))
+                self._graph.add((root_uri, FHIR[key], Literal(value)))
 
-    def generate(self) -> Graph:
+    def generate(self, limit=5) -> Graph:
         '''generator method for FhirGraph'''
-        self.add_data_to_graph(data=self._source)
+        i = 0
+        for entry in self._source['entry']:
+            resource = entry['resource']
+            self.add_data_to_graph(resource, None)
+            i += 1
+            if i > limit:
+                break
+        return self._graph
+        # self.add_data_to_graph(data=self._source)
